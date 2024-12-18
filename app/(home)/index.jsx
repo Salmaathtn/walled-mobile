811b50b46784fdc2343cbdf435f59d6f09f754a5
Salmaathtn <salmaathoetani@gmail.com>
2024-12-18 09:30:06 +0700

    fix: header for name and avatar

--- app/(home)/index.jsx
+++ app/(home)/index.jsx
@@ -7,12 +7,14 @@ import {
   StyleSheet,
   TouchableOpacity,
   ScrollView,
-  RefreshControl ,
+  RefreshControl,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import { StatusBar } from "expo-status-bar";
+
 
 function LogoTitle({ user }) {
   const [isAvatarActive, setIsAvatarActive] = useState(true);
@@ -31,6 +33,7 @@ function LogoTitle({ user }) {
 }
 
 export default function Home() {
+  const [transaction, setTransaction] = useState([]);
   const [user, setUser] = useState({
     fullname: "",
     id: "",
@@ -39,13 +42,12 @@ export default function Home() {
     wallet: { account_number: "", balance: "" },
   });
   const [refreshing, setRefreshing] = useState(false);
-  const onRefresh = useCallback(()=>{
+  const onRefresh = useCallback(() => {
     setRefreshing(true);
-    setTimeout(()=> {
+    setTimeout(() => {
       setRefreshing(false);
-    }, 2000)
-
-  })
+    }, 2000);
+  });
 
   useEffect(() => {
     const getData = async () => {
@@ -68,60 +70,64 @@ export default function Home() {
     };
     getData();
   }, []);
+  useEffect(() => {
+    const getTransaction = async () => {
+      try {
+        const value = await AsyncStorage.getItem("token");
+        if (value !== null) {
+          const res = await axios.get("http://172.20.10.3:8080/transactions", {
+            headers: {
+              Authorization: `Bearer ${value}`,
+            },
+          });
+          const transaction = res.data.data;
+          setTransaction(transaction);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getTransaction();
+  }, []);
   return (
     <ScrollView containerStyle={styles.container}>
-      <View style={styles.header} contentContainerStyle={styles.scrollView}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }>
-        {/* hapus */}
-        <View style={styles.header}></View>
+      <View style={styles.header}>
 
-        <View style={{ flexDirection: "row", alignItems: "center", gap: 10 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
           <LogoTitle user={user} />
           <View>
-            <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-              {user.fullname}
-            </Text>
-            <Text style={{ fontSize: 18 }}>Personal Account</Text>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', marginInline: 5 }}>{user.fullname}</Text>
+            <Text style={{ fontSize: 18, marginInline: 5 }}>Personal Account</Text>
           </View>
         </View>
-        <Image source={require("../../assets/suntoggle.png")} />
+
+        <Image source={require('../../assets/suntoggle.png')} />
+
       </View>
-      <View style={{ backgroundColor: "#FAFBFD", paddingHorizontal: 20 }}>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            paddingTop: 25,
-            justifyContent: "space-between",
-          }}
-        >
-          <View style={{ width: "70%" }}>
-            {user?.fullname && (
-              <Text style={{ fontSize: 18, fontWeight: "bold" }}>
-                Good Morning, {user.fullname}
-              </Text>
-            )}
-            <Text style={{ fontSize: 18 }}>
-              Check all your incoming and outgoing transactions here
-            </Text>
+      <View style={{ backgroundColor: '#FAFBFD', paddingHorizontal: 20 }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', paddingTop: 25, justifyContent: 'space-between' }}>
+          <View style={{ width: '70%' }}>
+            <Text style={{ fontSize: 22, fontWeight: 'bold', marginBottom: 8 }}>Good Morning, {user?.username}!</Text>
+            <Text style={{ fontSize: 16 }}>Check all your incoming and outgoing transactions here</Text>
           </View>
-          <Image
-            source={require("../../assets/Sun.png")}
-            style={{ width: 81, height: 77 }}
-          />
+          <Image source={require('../../assets/sun.png')} style={{ width: 81, height: 77 }} />
         </View>
 
         <View style={styles.accountnumber}>
-          <Text style={{ color: '#fff', fontSize: 18 }}>Account No.</Text>
-          {user.wallet?.account_number && <Text style={{ fontWeight: 'bold', color: '#fff', fontSize: 18 }}>{user.wallet?.account_number}</Text>}
+          <Text style={{ color: "#fff", fontSize: 18 }}>Account No.</Text>
+          {user.wallet?.account_number && (
+            <Text style={{ fontWeight: "bold", color: "#fff", fontSize: 18 }}>
+              {user.wallet?.account_number}
+            </Text>
+          )}
         </View>
 
         <View style={styles.balancebox}>
           <View>
             <Text style={{ fontSize: 20 }}>Balance</Text>
-            <Text style= {{fontSize : 24, fontWeight: 'bold'}}>Rp {user.wallet?.balance}</Text>
+            <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+              Rp {user.wallet?.balance}
+            </Text>
           </View>
           <View>
             <View style={{ gap: 20 }}>
@@ -172,9 +178,9 @@ export default function Home() {
           >
             Transaction History
           </Text>
-          {transactions.map((transaction) => (
+          {transaction?.map((tx) => (
             <View
-              key={transaction.id}
+              key={transaction?.id}
               style={{
                 flexDirection: "row",
                 alignItems: "center",
@@ -184,46 +190,47 @@ export default function Home() {
               }}
             >
               <View>
-                <Text style={{ fontSize: 18 }}>{transaction.name}</Text>
-                <Text style={{ fontSize: 16 }}>{transaction.type}</Text>
+                <Text style={{ fontSize: 19 }}>{user?.fullname}</Text>
+                <Text style={{ fontSize: 16 }}>{tx?.transaction_type}</Text>
                 <Text style={{ fontSize: 14, color: "#b3b3b3" }}>
-                  {transaction.date}
+                  {tx?.transaction_date}
                 </Text>
               </View>
               <Text
                 style={{
                   fontSize: 18,
-                  color: transaction.debit ? "red" : "green",
+                  color: tx?.transaction_type === "transfer" ? "red" : "green",
                 }}
               >
-                {transaction.debit ? "-" : "+"} Rp {transaction.amount}
+                {tx?.transaction_type === "transfer" ? "-" : "+"} Rp {tx.amount}
               </Text>
             </View>
           ))}
         </ScrollView>
       </View>
+      <StatusBar style="auto" />
     </ScrollView>
   );
 }
 
-const transactions = [
-  {
-    id: 1,
-    date: "08 December 2024",
-    amount: "75.000",
-    name: "Indoapril",
-    type: "Topup",
-    debit: false,
-  },
-  {
-    id: 2,
-    date: "06 December 2024",
-    amount: "80.000",
-    name: "Si Fulan",
-    type: "Transfer",
-    debit: true,
-  },
-];
+// const transactions = [
+//   {
+//     id: 1,
+//     date: "08 December 2024",
+//     amount: "75.000",
+//     name: "Indoapril",
+//     type: "Topup",
+//     debit: false,
+//   },
+//   {
+//     id: 2,
+//     date: "06 December 2024",
+//     amount: "80.000",
+//     name: "Si Fulan",
+//     type: "Transfer",
+//     debit: true,
+//   },
+// ];
 
 const styles = StyleSheet.create({
   container: {
@@ -273,13 +280,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   balancebox: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 20,
     borderRadius: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    shadowColor: '#000',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -288,15 +295,21 @@ const styles = StyleSheet.create({
     shadowRadius: 3.84,
     elevation: 5,
   },
+  avatarContainer: {
+    borderRadius: 9999, // Full circle
+    borderWidth: 4,
+    cursor: "pointer", // Optional for web
+    transition: "all 0.3s", // Optional for web
+  },
   accountnumber: {
     paddingHorizontal: 20,
     paddingVertical: 12,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: '#19918F',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#19918F",
     marginTop: 30,
     marginBottom: 40,
-    borderRadius: 10
+    borderRadius: 10,
   },
 });
